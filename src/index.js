06ae60c6397974a9662e7b2120e99f42bbf2da5d
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-10-31 09:39:30 +0100

    Inject lucky number const into HTML.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const name = "Sara";
+const luckyNumb = 13;
 
-ReactDom.render(<h1>Hi {name}</h1>, document.getElementById("root"));
+ReactDom.render(
+    <div>
+    <h1>Hi {name}</h1>
+    <p>Your lucky number is: {luckyNumb}</p>
+    </div>
+, document.getElementById("root"));
