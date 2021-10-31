64d44b49ed5d43dc68a9820f642791c4ef4f9110
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-10-31 09:46:43 +0100

    Final commit. Insert fName and lName const into the h1 HTML element.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const name = "Sara";
+const fName = "Srgl";
+const lName = "Brkchn";
 const luckyNumb = 13;
 
 ReactDom.render(
     <div>
-    <h1>Hi {name}</h1>
+    <h1>Hi {fName} {lName}!</h1>
     <p>Your lucky number is: {luckyNumb}</p>
     </div>
 , document.getElementById("root"));
